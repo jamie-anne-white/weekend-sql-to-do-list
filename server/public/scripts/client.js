04e67e6a20cba9fa4c3de0d62ca22737d8867946
Jamie Anne White <jamiewhite@Jamies-MBP.zyxel.com>
2020-10-19 11:41:35 -0500

    should  have been pushed lat night?

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     console.log('in jQuery');
     setupClickListeners();
     getTodo();
+    $('#view-todo').on('click', '.delete-btn', deleteTodo);
 
 
 });
@@ -12,15 +13,29 @@ function setupClickListeners() {
     $("#add-task-button").on('click', function () {
         console.log('button clicked');
         // get user input and put in an object
-    // let todoToSend = {
-    //     task_name: $('#task-name-in').val(),
-    //     task_type: $('#task-type-in').val(),
-    //     task_due: $('#task-due-in').val(),
-    //     task_status: $('#task-completed').val(),
-    //   };
-    //       // call saveTodo with the new object
-    //       console.log('todoToSend', todoToSend)
+        let todoToSend = {
+            task_name: $('#task-name-in').val(),
+            task_type: $('#task-type-in').val(),
+            task_due: $('#task-due-in').val(),
+            task_status: $('#task-completed').val(),
+          };
+              // call saveTodo with the new object
+              console.log('todoToSend', todoToSend)
     });
+}
+
+//GET TODO
+function postTodo(newTodo) {
+    $.ajax({
+        method: 'POST',
+        url: '/todo',
+        data: newTodo
+    }).then((response) => {
+        console.log('POST new todo');
+        getTodo();
+    }).catch(function (error) {
+        console.log(error);
+    })
 }
 
 //GET TODO
@@ -31,8 +46,71 @@ function getTodo() {
         type: 'GET',
         url: '/todo'
     }).then(function (response) {
-            console.log('in getTodo GET', response);
+            console.log('aaah', response);
+            appendTodo(response);
+           /*  //append to dom 
+            for (let i = 0; i < response.length; i++) {
+                $('#view-todo').append(`
+            <tr data-todo-id="${response[i].id}">
+                <td>${response[i].name}</td>
+                <td>${response[i].type}</td>
+                <td>${response[i].date}</td>
+                <td><button class ="delete-btn">DELETE</button></td>
+            `);
+
+            } */
         }).catch(function (error) {
             console.log('error in GET', error);
         });
     };
+
+function saveTodo(newTodo) {
+    console.log('in newTodo', newTodo);
+    // ajax call to server to get koalas
+    $.ajax({
+        method: 'POST',
+        url: '/todo',
+        data: newTodo
+    }).then(function (response) {
+        console.log('response', response);
+        getTodo();
+
+    }).catch(function (error) {
+        console.log('error in post', error);
+
+    });
+}
+
+    //APPEND TO DOM
+    function appendTodo(response) {
+        $('#view-todo').empty();
+        for (let i = 0; i < response.length; i++) {
+            let todo = response[i];
+            let $tr = $('<tr></tr>');
+            $tr.data('todo', todo.id);
+            $tr.append(`<td>${todo.task_name}</td>`);
+            $tr.append(`<td>${todo.task_type}</td>`);
+            $tr.append(`<td>${todo.task_due}</td>`);
+            $tr.append(`<td><button class ="completeBtn">COMPLETE</button></td>`);
+            $tr.append(`<td><button class ="deleteBtn">DELETE</button></td>`);
+        
+            $('#view-todo').append($tr);
+        };
+
+        function deleteTodo (){
+            let todoDelete = $(this).closest('tr').data('todo');
+            console.log(todoDelete);
+            $.ajax({
+               method: 'DELETE',
+               url: `/todo/${todoDelete}` //change the URL based on what you want to delete
+               //data: {id: idToDelete} will not work in DELETE
+           }).then(function (response) {
+               console.log(response);
+               getTodo;
+       
+           }).catch(function (error) {
+       
+           });
+   
+   
+        };
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -1,26 +1,50 @@
 const express = require('express');
-const router = express.Router();
-
-// DB CONNECTION
+// const { Router } = require('express');
+const todoRouter = express.Router();
 const pool = require('../modules/pool');
 
-
-// GET
-router.get('/', (req, res) => {
-  let queryText = 'SELECT * FROM "todo";';
+//GET
+todoRouter.get('/', (req, res) => {
+  let queryText = 'SELECT * FROM "todo" ORDER BY "id";';
   pool.query(queryText).then(result => {
     res.send(result.rows);
   })
   .catch(error => {
-    console.log('error getting todo GET', error);
+    console.log('error getting todo', error);
     res.sendStatus(500);
   });
 })
+//POST
+todoRouter.post('/', (req, res) => {
+    let newToDo= req.body;
+    console.log('Adding new todo:', newToDo);
+    let queryText = `
+    INSERT INTO "todo" ("task_name", "task_type", "task_due")
+    VALUES ($1, $2, $3);`;
+    pool.query(queryText, [newToDo.name, newToDo.type, newToDo.due])
+    .then(result => {
+      res.sendStatus(201);
+    })
+    .catch(error => {
+      console.log(`Error adding new todo`, error); 
+      res.sendStatus(500);
+    });
+}); 
 
-// POST
+//delete
+todoRouter.delete('/:id', (req, res) => {
+  let reqId = req.params.id;
+  console.log('Delete request for id', reqId);
+  let sqlText = 'DELETE FROM "todo" WHERE "id"=$1;';
+  pool.query(sqlText, [req.params.id])
+    .then( (result) => {
+      console.log('todo deleted');
+      res.sendStatus(200);
+    })
+    .catch( (error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500); // Good server always responds
+    })
+});
 
-
-// DELETE
-
-
-module.exports = router;
+module.exports = todoRouter;
